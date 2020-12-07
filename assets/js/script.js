71c73b31de59f1410ed27a2c4f29b3840bd0fc89
rodionovkost <rodionovkost1811@gmail.com>
2020-12-07 12:56:34 +0300

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,9 @@ function city(){
 function knowledge_lang(){
 	$('#knowledge_lang').css('left','0');
 }
+function success_bitrix(){
+    $('#success_bitrix').css('left','0');
+}
 function html5(){
 	$('#html5').css('opacity','1');
 }
@@ -28,6 +31,12 @@ function js(){
 }
 function jquery(){
 	$('#jquery').css('opacity','1');
+}
+function ajax(){
+    $('#ajax').css('opacity','1');
+}
+function php(){
+    $('#php').css('opacity','1');
 }
 $(document).ready(function(){
 	$('.main').show(1500,function(){
@@ -44,15 +53,13 @@ $(window).scroll(function() {
     	$('#inst').css('right','80px');
     	$('#vk').css('right','125px');
     	$('#telegram').css('right','165px');
-    	$('#gmail').css('right','205px');
     }
     else if($top <= 100){
-    	$('.second_block h1').css('left','-1500px');
+    	$('.second_block h1').css('left','-100%');
     	$('.top_menu').css('background','rgba(16, 78, 131,0)');
     	$('#inst').css('right','-100px');
     	$('#vk').css('right','-100px');
     	$('#telegram').css('right','-100px');
-    	$('#gmail').css('right','-100px');
     	$('#lang').css('right','-100px');
     }
     if ( $top >= 200 ) {
@@ -60,8 +67,8 @@ $(window).scroll(function() {
     	$('#all_projects').css('left','0');
     }
     else if($top <= 200){
-    	$('.second_block .project').css('left','-1500px');
-    	$('#all_projects').css('left','-1500px');
+    	$('.second_block .project').css('left','-110%');
+    	$('#all_projects').css('left','-100%');
     }
     if ( $top >= 900 ) {
     	$('.about').css('opacity','1');
@@ -71,7 +78,7 @@ $(window).scroll(function() {
     	setTimeout(experience,1500);
     	setTimeout(education,2000);
     	setTimeout(knowledge_lang,2500);
-    	
+    	setTimeout(success_bitrix,3000);
     }
     else if($top <= 900){
     	$('.about').css('opacity','0');
@@ -83,50 +90,11 @@ $(window).scroll(function() {
     	setTimeout(css3,1500);
     	setTimeout(js,2000);
     	setTimeout(jquery,2500);
+        setTimeout(ajax,3000);
+        setTimeout(php,3500);
     }
     else if($top <= 1300){
     	$('.skill_block').css('opacity','0');
-    	$('.skill_block h1').css('right','-1500px');
+    	$('.skill_block h1').css('right','-100%');
     }
-    /* else if ( $top <= 100 ) {
-	  $('.topmenu').css('width','103%');
-      $('.topmenu').css('background','rgba(0,0,0,0)');
-      $('.logoname').css('color','white');
-      $('.logodvig').css('background','rgba(0,0,0,0)');
-      $('.logodvig').css('padding','0');
-      $('#logo').css('width','2.5vmax');
-      $('.topmenu').css('box-shadow','0 0 0px rgba(0,0,0,0.5)');
-      $('.itemsmenu li b').css('color','lightgray');
-      $(".itemsmenu li b")
-      	.mouseenter(function() {
-      		if ($top >= 100) {
-    			$(this).css("color", "Dimgrey");
-    		}
-    		else{
-    			$(this).css("color", "white");
-    		}
-		})
-      	.mouseleave(function() {
-      		if ($top >= 100){
-      			$(this).css("color", "Dimgrey");
-      		}
-     		else{
-     			$(this).css("color", "lightgrey");
-     		}
-		});
-    }
-    if ($top >=500 && $top<=1400) {
-    	$('#item1 b').css('color','black');
-    	$('#item1').css('border-bottom','3px solid teal');
-    	$('#item2').css('border-bottom','0px solid teal');
-    }
-    else if ($top >=1400) {
-    	$('#item1').css('border-bottom','0px solid teal');
-    	$('#item2 b').css('color','black');
-    	$('#item2').css('border-bottom','3px solid teal');
-    }
-    else{
-    	$('#item1').css('border-bottom','0px solid teal');
-    	$('#item2').css('border-bottom','0px solid teal');
-    }*/
   });
